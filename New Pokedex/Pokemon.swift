a9854824aaa162ee899e68b6e6948a7dea168024
allangomesptc <allan@Allans-MBP.home>
2016-02-21 14:15:15 +0000

    collection view customized

---
+++ New Pokedex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  New Pokedex
+//
+//  Created by Allan Alvaro Gomes on 21/02/16.
+//  Copyright © 2016 allan. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int){
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
