117b86fbfee7bac7ab987eb793657cb1e9fcfd48
Allan Gomes <allanalvaro@gmail.com>
2016-02-23 22:47:53 +0000

    json parsing

---
+++ New Pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  New Pokedex
+//
+//  Created by Allan Alvaro Gomes on 23/02/16.
+//  Copyright © 2016 allan. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon"
+
+typealias DownloadComplete = () -> ()
--- New Pokedex/PokemonDetailVC.swift
+++ New Pokedex/PokemonDetailVC.swift
@@ -10,15 +10,37 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
 
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    
+    
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameLbl.text = pokemon.name
+        mainImg.image = UIImage(named: "\(pokemon.pokedexId)")
+        
+        pokemon.downloadPokemonDetails { () -> () in
+            //this will be called after download is done.
+        }
+        
 
     }
 
- 
-
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 
 
